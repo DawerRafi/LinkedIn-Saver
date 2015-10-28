2dc5ebe6d82e189db7f28ae1dcf2eae4fbdc4e77
DawerRafi <dawer.rafi@aiesec.net>
2015-10-28 12:40:18 +0500

    Updated Content script

--- LinkedInSaver v1.0/content_scriptt2.js
+++ LinkedInSaver v1.0/content_scriptt2.js
@@ -3,55 +3,26 @@ function csvFile(str){
 this.obj =str;
 }
 
+/*
+*creating csv file and initailizing attributes
+*/
 var csv= new csvFile("data:text/csv;charset=utf-16,");
 var csvcontent="data:text/csv;charset=utf-16,";
 var loopURL=document.URL;
 var counter=1;
 httpGet(document.URL,csv);
-/*
- function call(xmlHttp) {
-						  coded  = this.responseXML.getElementsByTagName("code")[0].childNodes[0].nodeValue;
-		var js=JSON.parse(coded);
-			var x = new Array(10);
-			for (var i = 0; i < 10; i++) {
-				x[i] = new Array(2);
-			}
-			var j=0;
-		for(var i=0;i<js.content.page.voltron_unified_search_json.search.results.length;i++){
-			if(js.content.page.voltron_unified_search_json.search.results[i].person){
-				
-				x[j][0]=js.content.page.voltron_unified_search_json.search.results[i].person.fmt_name;
-				x[j][1]=js.content.page.voltron_unified_search_json.search.results[i].person.fmt_headline;
-				j++;
-			}
-				
-		}
-			x.forEach(function(infoarray, index){
-				 datastring = infoarray.join(",");
-				 csvcontent+=index < x.length ? datastring+ "\n" : datastring;
-				 csv.obj += index < x.length ? datastring+ "\n" : datastring;
-				 }); 
-		if(js.content.page.voltron_unified_search_json.search.baseData.resultPagination.nextPage){
-			loopURL="https://www.linkedin.com"+js.content.page.voltron_unified_search_json.search.baseData.resultPagination.nextPage.pageURL;
-			console.log(loopURL);
-			httpGet(loopURL,csv,call());
-		}
-		else{
-			download(csvcontent);
-			}
-} //} 
-*/
-//download(csvcontent);
-	//function getResponse(){
-	//	return function(){ coded  = this.responseXML.getElementsById("voltron_srp_main-content")[0].childNodes[0].nodeValue;};
-//	}
-		//download(csv);
-		//download(csvcontent);
-  // div.innerHTML=xmlHttp.responseText;
-    
-//var coded=div.getElementsByTagName('code');
-//download("file.txt",coded.InnerHTML + " CONTENT STARTING FROM HERE : "+content);
 
+/*
+* The main function which takes in json from the linkedin source code and parses it to get three things:
+* 	-Name of the person
+*	-Designation of the person
+*	-Company of the person
+*
+* The function is recursive in nature. I.e it first scraps one page of the search result and if the next page
+* is present, it recurses for the next page till it has scrapped all of the pages.
+* The function makes asynchronous calls to the search results, gets the json formatted data and then retrieved people
+* from there.
+*/
 function httpGet(theUrl,csv)
 {
     var xmlHttp = null;
@@ -61,119 +32,127 @@ function httpGet(theUrl,csv)
 	xmlHttp.responseType = "document";
 	xmlHttp.onreadystatechange=function()
 	{
-		try{
-		if (xmlHttp.readyState==4 && xmlHttp.status==200)
-		{ 
-		coded  = xmlHttp.responseXML.getElementsByTagName("code")[0].childNodes[0].nodeValue;
-		try{
-		var js=JSON.parse(coded);
-    }catch(e){
-		coded=coded.replace(/:\\u002d1,/g,":\"NoName\",");
-		try{
-		var js=JSON.parse(coded);
-		}
-		catch(ee){
+		try
+		{
+			if (xmlHttp.readyState==4 && xmlHttp.status==200)
+			{ 
+			coded  = xmlHttp.responseXML.getElementsByTagName("code")[0].childNodes[0].nodeValue;
+			try
+			{
+				var js=JSON.parse(coded);
+			}
+			catch(e)
+			{
+				coded=coded.replace(/:\\u002d1,/g,":\"NoName\",");
+				try
+				{
+					var js=JSON.parse(coded);
+				}
+				catch(ee)
+				{
+					alert('Encountered a Parsing Error.Press OK to download the successfully parsed content');
+					chrome.runtime.sendMessage({greeting: "WorkDone"}, function(response) {
+					console.log(response.farewell);
+					});
+					download(csvcontent);
+					console.log(e);
+				}
+			}
 			
-		alert('Encountered a Parsing Error.Press OK to download the successfully parsed content');
-		chrome.runtime.sendMessage({greeting: "WorkDone"}, function(response) {
-	console.log(response.farewell);
-	});
-        download(csvcontent);
-		console.log(e);
-		//downloadErr("ErrorFile.txt",coded);
-		}
-		
-    }
 			var x = new Array(10);
 			for (var i = 0; i < 10; i++) {
 				x[i] = new Array(4);
 			}
+			
 			var j=0;
-		for(var i=0;i<js.content.page.voltron_unified_search_json.search.results.length;i++){
-			if(js.content.page.voltron_unified_search_json.search.results[i].person){
-				x[j][0]=js.content.page.voltron_unified_search_json.search.results[i].person.firstName;
-				x[j][1]=js.content.page.voltron_unified_search_json.search.results[i].person.lastName;
 			
-				if(js.content.page.voltron_unified_search_json.search.results[i].person.fmt_name="" || !js.content.page.voltron_unified_search_json.search.results[i].person.fmt_name){
-				x[j][0]="LinkedIn";
-				x[j][1]="User";
-				}
-				x[j][0]=x[j][0].replace(/<[ -~]*?>/g," ");
-				x[j][1]=x[j][1].replace(/<[ -~]*?>/g," ");
+			//looping through all the persons in the current page
+			for(var i=0;i<js.content.page.voltron_unified_search_json.search.results.length;i++){
+				if(js.content.page.voltron_unified_search_json.search.results[i].person){
+					x[j][0]=js.content.page.voltron_unified_search_json.search.results[i].person.firstName;
+					x[j][1]=js.content.page.voltron_unified_search_json.search.results[i].person.lastName;
+					
+					//If name is not shown by linked in then we hardcode it
+					if(js.content.page.voltron_unified_search_json.search.results[i].person.fmt_name="" || !js.content.page.voltron_unified_search_json.search.results[i].person.fmt_name){
+						x[j][0]="LinkedIn";
+						x[j][1]="User";
+					}
+					//removing extra xml used for font styling by linkedin
+					x[j][0]=x[j][0].replace(/<[ -~]*?>/g," ");
+					x[j][1]=x[j][1].replace(/<[ -~]*?>/g," ");
 				
-				//x[j][1]=js.content.page.voltron_unified_search_json.search.results[i].person.fmt_headline;
-				var str=js.content.page.voltron_unified_search_json.search.results[i].person.fmt_headline;
-				var two = str.split(" at ");
-				x[j][2]=two[0].replace(/&amp;|,|<[ -~]*?>/g," ");
-				if(two[1]){
-				x[j][3]=two[1].replace(/&amp;|,|<[ -~]*?>/g," ");
+					//parsing designation and company of the individual
+					var str=js.content.page.voltron_unified_search_json.search.results[i].person.fmt_headline;
+					var two = str.split(" at ");
+					x[j][2]=two[0].replace(/&amp;|,|<[ -~]*?>/g," ");
+					if(two[1]){
+						x[j][3]=two[1].replace(/&amp;|,|<[ -~]*?>/g," ");
+					}
+					j++;
 				}
-				j++;
 			}
-			
-				
+			//showing alert if no search results found
+			if(j==0)
+			{
+				alert("0 results found");
+			}
+			//parsing the next result page
+			else
+			{	
+				//saving the scrapped data
+				x.forEach(function(infoarray, index){
+					datastring = infoarray.join(",");
+					csvcontent+=index < x.length ? datastring+ "\n" : datastring;
+					csv.obj += index < x.length ? datastring+ "\n" : datastring;
+				}); 
+				//if next page is available
+				if(js.content.page.voltron_unified_search_json.search.baseData.resultPagination.nextPage)
+				{
+					loopURL="https://www.linkedin.com"+js.content.page.voltron_unified_search_json.search.baseData.resultPagination.nextPage.pageURL;
+					counter +=1;
+					chrome.runtime.sendMessage({greeting: "PageRefresh",OnPage: counter+""}, function(response) {});
+					console.log(loopURL);
+					//recurse back to this function
+					httpGet(loopURL,csv);
+				}
+				//download the file for user
+				else
+				{
+					chrome.runtime.sendMessage({greeting: "WorkDone"}, function(response) 
+					{
+					console.log(response.farewell);
+					});
+					download(csvcontent);
+				}
+			}
+		
+			}
+			else{
+				//return;
+			}
 		}
-		if(j==0){
-			alert("0 results found");
-		}
-		else{
-			x.forEach(function(infoarray, index){
-				 datastring = infoarray.join(",");
-				 csvcontent+=index < x.length ? datastring+ "\n" : datastring;
-				 csv.obj += index < x.length ? datastring+ "\n" : datastring;
-				 }); 
-		if(js.content.page.voltron_unified_search_json.search.baseData.resultPagination.nextPage){
-			loopURL="https://www.linkedin.com"+js.content.page.voltron_unified_search_json.search.baseData.resultPagination.nextPage.pageURL;
-			counter +=1;
-			chrome.runtime.sendMessage({greeting: "PageRefresh",OnPage: counter+""}, function(response) {
-				
-			});
-			console.log(loopURL);
-			httpGet(loopURL,csv);
-		}
-		else{
+
+		catch(Ex)
+		{
+			alert("An Error Occured "+Ex);
 			chrome.runtime.sendMessage({greeting: "WorkDone"}, function(response) {
 			console.log(response.farewell);
 			});
-			download(csvcontent);
-			}
-		
 		}
-		
-		//var data = [["name1", "city1", "some other info"], ["name2", "city2", "more info"]];
-		}
-	else{
-		//return;
-	}
-	/*
-	
-	*/
-	}
-
-	catch(Ex){
-		alert("An Error Occured "+Ex);
-		
-		chrome.runtime.sendMessage({greeting: "WorkDone"}, function(response) {
-	console.log(response.farewell);
-	});
-	}
-};
+	};
 	xmlHttp.send(null);
 
 }
+/*
+This function takes in the scrapped data and returns a file to be downloaded by the user.
+*/
 function download(csv) {
  	 var person = prompt("Please specify the filename you wish to create", "mydata.csv");
   
+	
 	 var encodeduri = encodeURI(csv);
 	 var link = document.createElement("a");
 	 
-	 
-	 /*
-	 var output="";
-	 for(var y=0;y<x.length;y++){
-		output+= x[y][0]+" , "+x[y][1]+"\n";
-	 }
-	 */
 	 
 	 
 	 link.setAttribute("href", encodeduri);
@@ -193,17 +172,3 @@ function download(csv) {
 	 link.click();
 }
 
-function downloadErr(filename, text) {
-    var pom = document.createElement('a');
-    pom.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
-    pom.setAttribute('download', filename);
-
-    if (document.createEvent) {
-        var event = document.createEvent('MouseEvents');
-        event.initEvent('click', true, true);
-        pom.dispatchEvent(event);
-    }
-    else {
-        pom.click();
-    }
-}
